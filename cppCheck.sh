b7069f154ec46e2ca270ddd8bed2d1cb8f42dd82
andrea <giovacchini.andrea@konvergence.it>
2018-06-11 12:57:02 +0200

    Pulito progetto

--- cppCheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cppcheck --library=posix.cfg -j 4 --enable=all --suppress=missingIncludeSystem --xml --verbose -f -I /usr/include -I /usr/local/include -I /usr/local/include/boost ./ 2> err.xml
-cppcheck-htmlreport --file err.xml --report-dir="CodeAnalysisReport" --title="PromoCalculator"
-
-<--- When an object of a class is created, the constructors of all member variables are called consecutively in the order the variables are declared, even if you don't explicitly write them to the initialization list. You could avoid assigning 'description' a value by passing the value to the constructor in the initialization list.
-}
